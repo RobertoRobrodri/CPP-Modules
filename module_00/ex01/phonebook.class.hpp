5bd91eaa6d4fddea17547999fb11c3ba975d65d4
Roberto Rodriguez garcia <robrodri@c2r1s3.42madrid.com>
2021-11-12 13:02:37 +0100

    Ex01 starting

---
+++ module_00/ex01/phonebook.class.hpp
@@ -0,0 +1,17 @@
+#ifndef PHONEBOOK_CLASS_H
+# define PHONEBOOK_CLASS_H
+
+class	phonebook {
+
+	public:
+
+//	Array de contactos
+
+	phonebook (void);
+	~phonebook (void);
+
+	private:
+
+	const int	_max_contact;
+};
+#endif
--- module_00/ex01/phonebook.cpp
+++ module_00/ex01/phonebook.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include "phonebook.class.hpp"
 
-int main(int argc, char **argv)
+// * Declarar constantes las funciones que no cambian las intances de las clases
+
+int main(void)
 {
-	
+	std::string str;
+
+	std::cout << "Input a command" << std::endl;
+	std::cout << "Available commands: ADD, SEARCH, EXIT" << std::endl;
+	while (1)
+	{
+		std::cin >> str;
+		if (!str.compare("EXIT"))
+			break ;
+		else if (!str.compare("ADD"))
+			std::cout << "ADD" << std::endl;
+		else if (!str.compare("SEARCH"))
+			std::cout << "SEARCH" << std::endl;
+		else
+			std::cout << "Invalid command" << std::endl;
+	}
+	return (0);
 }

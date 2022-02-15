1587d6ca62d655b8d0290cd1d38c69ec6baa35ab
Anaximandro-Robrodri <karmmero@gmail.com>
2022-02-15 21:35:21 +0100

    guess it´s done module 05

--- module_05/ex03/Intern.cpp
+++ module_05/ex03/Intern.cpp
@@ -11,26 +11,26 @@ Intern::~Intern(void) {
 	std::cout << "Intern destructor called" << std::endl;
 }
 
-PresidentialPardon Intern::makePresidentialPardon(std::string target) {
-	PresidentialPardon PP(target);
+Form *Intern::makePresidentialPardon(std::string target) {
+	Form *PP = new PresidentialPardon(target);
 	return PP;
 }
 
-RobotForm Intern::makeRobotRequest(std::string target) {
-	RobotForm Roboto(target);
+Form *Intern::makeRobotRequest(std::string target) {
+	Form *Roboto = new RobotForm(target);
 	return Roboto;
 }
 
-AsciiTree Intern::makeTreeForm(std::string target) {
-	AsciiTree Tree(target);
+Form *Intern::makeTreeForm(std::string target) {
+	Form *Tree = new AsciiTree(target);
 	return Tree;
 }
 
 Form	*Intern::makeForm(std::string name, std::string target) {
-	Form (Intern::*func[])() = {&Intern::makePresidentialPardon, &Intern::makeRobotRequest, &Intern::makeTreeForm};
+	Form *(Intern::*func[])(std::string) = {&Intern::makePresidentialPardon, &Intern::makeRobotRequest, &Intern::makeTreeForm};
 	std::string	form_types[3] = {"robotomy request", "shrubbery creation", "presidential pardon"};
 	for (int i = 0; i < 3; i++)
 		if (name.compare(form_types[i]) == 0)
-			return func[i](target);
+			return (this->*(func[i]))(target);
 	throw Intern::NotExistingForm();
 }

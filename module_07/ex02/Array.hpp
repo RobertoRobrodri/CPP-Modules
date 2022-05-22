25dfc3c0b022980df6142ba08f05c4ab511b20ee
Anaximandro-Robrodri <karmmero@gmail.com>
2022-05-22 10:16:42 +0200

    Made deep copy with 0 leaks

--- module_07/ex02/Array.hpp
+++ module_07/ex02/Array.hpp
@@ -17,22 +17,29 @@ class	Array {
 		Array ( unsigned int n ) : _size(n) {
 			this->array = new T[n];
 		};
-		Array ( const Array & var ) {
+		Array ( const Array & var ) : array(NULL) {	
 			*this = var;
 		}
-		~Array ( void ) {}
+		~Array ( void ) {
+			delete[] this->array;
+		}
 
 		Array & operator=(Array const &ar) {
-			this->array = ar.array;
+			if (this != &ar)
+			{
+				this->_size = ar._size;
+				if (this->array != NULL)
+					delete[] this->array;
+				this->array = new T[ar._size];
+				for (int i = 0; i < this->_size; i++)
+					this->array[i] = ar.array[i];
+			}
 			return *this;
 		}
 
 		T & operator[](int index) {
 			if (index >= this->size() || index < 0)
-			{
-				std::cout << index << std::endl;
 				throw std::exception();
-			}
 			return this->array[index];
 		}
 
--- module_07/ex02/main.cpp
+++ module_07/ex02/main.cpp
@@ -16,6 +16,7 @@ int main(int, char**)
     {
         Array<int> tmp = numbers;
         Array<int> test(tmp);
+        test = numbers;
     }
 
     for (int i = 0; i < MAX_VAL; i++)

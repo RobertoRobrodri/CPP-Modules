360b6a2d04b65339e296cd258d256bf54a980b5d
Anaximandro-Robrodri <karmmero@gmail.com>
2022-07-26 22:53:31 +0200

    It was good already, now it should just be ok

--- module_07/ex00/main.cpp
+++ module_07/ex00/main.cpp
@@ -1,6 +1,6 @@
 # include "whatever.hpp"
 
-int main (void)
+/*int main (void)
 {
 	int x = 5, y = 10;
 	std::cout << max<int>(x, y) << std::endl;
@@ -34,4 +34,98 @@ int main (void)
 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 
 	return 0;
+}*/
+
+class Foo
+{
+	public:
+		Foo(void)
+			: _name(""), _i(0), _c(0)
+		{};
+
+		Foo(std::string const name, int const i, char const c)
+			: _name(name), _i(i), _c(c)
+		{};
+
+		Foo(Foo const &foo)
+			: _name(foo._name), _i(foo._i), _c(foo._c)
+		{};
+
+		Foo	&operator=(Foo const &rhs)
+		{
+			this->_i = rhs._i;
+			this->_c = rhs._c;
+			return (*this);
+		}
+
+		bool operator==(Foo const &rhs) const
+		{
+			return (this->_i + this->_c == rhs._i + rhs._c);
+		}
+
+		bool	operator<(Foo const &rhs) const
+		{
+			return (this->_i + this->_c < rhs._i + rhs._c);
+		}
+
+		bool	operator<=(Foo const &rhs) const
+		{
+			return (*this < rhs || *this == rhs);
+		}
+
+		bool	operator>(Foo const &rhs) const
+		{
+			return (!(*this <= rhs));
+		}
+
+		bool	operator>=(Foo const &rhs) const
+		{
+			return (!(*this < rhs));
+		}
+
+		std::string	getName(void) const {return (this->_name);}
+		int			getInt(void) const {return (this->_i);}
+		char		getChar(void) const {return (this->_c);}
+
+	private:
+		std::string const	_name;
+		int					_i;
+		char				_c;
+};
+
+std::ostream	&operator<<(std::ostream &out, Foo const &rhs)
+{
+	out << rhs.getName() << " I:" << rhs.getInt() << " C:" << rhs.getChar();
+	return (out);
+}
+
+template <typename T>
+void	test(T &first, T &second)
+{
+	std::cout << "First: " << first << std::endl
+		<< "Second: " << second << std::endl;
+	std::cout << "Max: " << ::max<T>(first, second) << std::endl;
+	std::cout << "Min: " << ::min<T>(first, second) << std::endl;
+	std::cout << "Swap" << std::endl;
+	::swap<T>(first, second);
+	std::cout << "First: " << first << std::endl
+		<< "Second: " << second << std::endl;
+}
+
+int	main(void)
+{
+	int			i1 = -1;
+	int			i2 = 2;
+	char		c1 = '*';
+	char		c2 = '2';
+	Foo			f1("First", 0, '*');
+	Foo			f2("Second", 50, '\a');
+	std::string	s1 = "chaine1";
+	std::string	s2 = "chaine2";
+
+	test<int>(i1, i2);
+	test<char>(c1, c2);
+	test<Foo>(f1, f2);
+	test<std::string>(s1, s2);
+	return (111);
 }
--- module_07/ex01/iter.hpp
+++ module_07/ex01/iter.hpp
@@ -4,18 +4,14 @@
 # include <string>
 
 template < typename T >
-void	iter(T* const &array, size_t length, void (*f) (T &var))
+void	iter(T* array, size_t const length, void (*f) (T const))
 {
+	if (!f)
+		return ;
 	for (size_t i = 0; i < length; i++)
 	{
 		f(array[i]);
 	};
-}
-
-template < typename T >
-void	print(T &var)
-{
-	std::cout << var << std::endl;
 }
 
 #endif

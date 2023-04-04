7d26fcfee4439841ca31d9e54fe1337655d6d132
Roberto Rodríguez García <84873843+RobertoRobrodri@users.noreply.github.com>
2023-04-04 12:39:39 +0200

    Add files via upload

--- module_09/ex01/RPN.hpp
+++ module_09/ex01/RPN.hpp
@@ -2,18 +2,27 @@
 #define RPN_HPP
 #include <string>
 #include <iostream>
+#include <sstream>
+#include <stack>
 
 class	RPN {
 
 	private:
 
+		void		get_value(void);
+
 	public:
 
+		std::stack<int>	stack;
+		char			operation_type;
+
 		RPN 			( void );
-		RPN 			( std::string str );
+	//	RPN 			( std::string str );
 		RPN 			( const RPN & var );
 		~RPN			( void );
 		RPN &operator=	(const RPN &tmp);
+
+		void			calculate(std::string calculus);
 };
 std::ostream &operator<<(std::ostream& os, const RPN &tmp);
 
